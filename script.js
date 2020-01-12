8243412a9429f490758b5947926597058ac876a3
Vishnu Gunapathi <38277626+Twinji@users.noreply.github.com>
2020-01-12 23:28:08 +1030

    Create canvas update loop

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onload = function(e) {
+
+    // canvas setup
+    var canvas = document.getElementById("canvas");
+    var c = canvas.getContext("2d");
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+
+    // initial setup
+    init(c);
+    
+    // main logic loop
+    var loop = function() {
+        update();
+        render(c);
+        window.requestAnimationFrame(loop, canvas);
+    }
+    window.requestAnimationFrame(loop, canvas);
+}
+
+function init(c) {}
+
+function update() {}
+
+function render(c) {}
